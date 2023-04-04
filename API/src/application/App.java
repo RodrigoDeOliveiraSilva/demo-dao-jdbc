cf9907b185ce399670c6b44cd802454916f95d5c
Rodrigo de Oliveira Silva <rodrigodeoliveirasilva20@gmail.com>
2023-04-04 19:58:24 +0000

    Created JDBC Database Access Class

--- API/src/application/App.java
+++ API/src/application/App.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.sql.Connection;
 import java.util.Date;
 
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -11,9 +13,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         Department dp = new Department(1, "Books");
 
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+
         Seller seller = new Seller(21,"Bob","bob@gmail.com", new Date(),3000.0, dp);
         
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
